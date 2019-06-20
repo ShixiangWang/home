96d0279d22c03ad928a960836a2de2befc07e6f8
ShixiangWang <wangshx@shanghaitech.edu.cn>
2019-06-20 16:00:31 +0800

    增强post模板功能；添加sci写作笔记

--- new.R
+++ new.R
@@ -1,40 +1,56 @@
-################
-## 用rmd写博客 ##
-################
-# 作者：王诗翔
-new_post <- function(post_name=NULL, dir=file.path(getwd(), "content/cn/post/"),
-                     type=c('post'),
-                     template_path = getwd(), add_prefix=TRUE, edit_file=TRUE,
-                     force=FALSE){
-  if(is.null(post_name)){
+# Create posts automatically
+# Author: Shixiang Wang
+# License: MIT
+new_post <- function(post_name = NULL, dir = file.path(getwd(), "content/cn/post/"),
+                     type = c("rmd", "md"),
+                     template_path = getwd(), add_prefix = TRUE, edit_file = TRUE,
+                     force = FALSE) {
+  if (is.null(post_name)) {
     stop("A post name must be given!")
   }
 
   type <- match.arg(type)
-  if (type == "post"){
-    template_name <- "template_post_cn.Rmd"
+  if (type == "rmd") {
+    template_name <- "template_post.Rmd"
+  } else if (type == "md") {
+    template_name <- "template_post.md"
   } else {
     stop("Not supported!")
   }
 
-  input_file      <- file.path(template_path, template_name)
+  input_file <- file.path(template_path, template_name)
   if (add_prefix) {
     current_time <- Sys.Date()
-    out_file     <- paste0(dir, current_time, "-", post_name, ".Rmd")
+    if (type == "rmd") {
+      out_file <- file.path(dir, paste0(current_time, "-", post_name, ".Rmd"))
+    } else {
+      out_file <- file.path(dir, paste0(current_time, "-", post_name, ".md"))
+    }
   } else {
-    out_file     <- paste0(dir, post_name, ".Rmd")
+    if (type == "rmd") {
+      out_file <- file.path(dir, paste0(post_name, ".Rmd"))
+    } else {
+      out_file <- file.path(dir, paste0(post_name, ".md"))
+    }
   }
 
   if (file.exists(out_file)) {
     if (!force) stop("File exists, use force=TRUE if you make sure rewrite it.")
   }
   message("Copying contents of ", input_file, " to ", out_file)
-  fl_content     <- readLines(input_file)
+  fl_content <- readLines(input_file)
   # Modify contents in template
   fl_content <- ifelse(grepl("date:", fl_content), paste0("date:", " \"", Sys.Date(), "\""), fl_content)
+  if (type == "md") {
+    fl_content <- ifelse(grepl("lastmod:", fl_content), paste0("lastmod:", " \"", Sys.Date(), "\""), fl_content)
+  }
   writeLines(fl_content, out_file)
   if (edit_file) {
     file.edit(out_file)
   }
-  message("New Rmarkdown post creat successfully!")
+  if (type == "rmd") {
+    message("Create new Rmarkdown post successfully!")
+  } else {
+    message("Create new markdown post successfully!")
+  }
 }
---
+++ template_post.md
@@ -0,0 +1,9 @@
+---
+title: "Put your title here"
+author: "王诗翔"
+date: '2018-08-10'
+lastmod: '2018-08-10'
+slug: ""
+---
+
+
