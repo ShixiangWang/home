0c94f09676ed31260c5f94d774035d8f6c3eb11a
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-09-04 00:13:11 +0800

    Update tour

--- gomod/tour/main.go
+++ gomod/tour/main.go
@@ -7,8 +7,19 @@ import (
 
 func main() {
 	var name string
-	flag.StringVar(&name, "name", "Go tour", "help info")
 	flag.Parse()
+	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+	goCmd.StringVar(&name, "name", "Golang", "Help info")
+	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+	phpCmd.StringVar(&name, "n", "Php", "Help info")
+
+	args := flag.Args()
+	switch args[0] {
+	case "go":
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		_ = phpCmd.Parse(args[1:])
+	}
 
 	log.Printf("name: %s", name)
 }
