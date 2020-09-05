3376e2273b15d33a338a71c048280a8408823a82
Shixiang Wang <wangshx@shanghaitech.edu.cn>
2020-09-05 23:51:04 +0800

    添加单词转换工具的学习

--- gomod/tour/go.mod
+++ gomod/tour/go.mod
@@ -3,6 +3,6 @@ module github.com/ShixiangWang/home/gomod/tour
 go 1.14
 
 require (
-	github.com/spf13/cobra v1.0.0 // indirect
+	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.5 // indirect
 )
--- gomod/tour/main.go
+++ gomod/tour/main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"flag"
 	"log"
+
+	"github.com/ShixiangWang/home/gomod/tour/cmd"
 )
 
 func main() {
-	var name string
-	flag.Parse()
-	goCmd := flag.NewFlagSet("go", flag.ExitOnError)
-	goCmd.StringVar(&name, "name", "Golang", "Help info")
-	phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
-	phpCmd.StringVar(&name, "n", "Php", "Help info")
-
-	args := flag.Args()
-	switch args[0] {
-	case "go":
-		_ = goCmd.Parse(args[1:])
-	case "php":
-		_ = phpCmd.Parse(args[1:])
+	err := cmd.Execute()
+	if err != nil {
+		log.Fatalf("cmd.Execute err: %v", err)
 	}
-
-	log.Printf("name: %s", name)
 }
